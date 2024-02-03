ea80ca411a33c12961db4e4fa2bd1cf9b1284f6f
mustafashahzadd <135046795+mustafashahzadd@users.noreply.github.com>
2024-02-03 13:20:30 +0500

    CA - Application project added

---
+++ CleanMovie.Application/IMovieService.cs
@@ -0,0 +1,19 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    
+    // This is a use case
+    /*
+     This Interface is responsible for defining the buisness case and how this cas eis implemented
+     */
+    public interface IMovieService
+    {
+        List<Movie> GetAllMovies();
+    }
+}
---
+++ CleanMovie.Application/MovieService.cs
@@ -0,0 +1,25 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository movie_repository;
+        //Constuctor Dependency injection
+        public MovieService(IMovieRepository movie_repository)
+        {
+            this.movie_repository = movie_repository;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+           var movies= movie_repository.GetAllMovies();
+            return movies;
+        }
+    }
+}
