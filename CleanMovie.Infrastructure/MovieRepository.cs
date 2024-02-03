57871c55a562438377cef66c5b055fdb050afa27
mustafashahzadd <135046795+mustafashahzadd@users.noreply.github.com>
2024-02-03 13:22:40 +0500

    CA - Infrastructure project added

---
+++ CleanMovie.Infrastructure/MovieRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Infrastructure
+{
+    public class MovieRepository : IMovieRepository
+    {
+        //At this moment we will be hardcoding the data
+        public static List<Movie> movies = new List<Movie>()
+        {
+            new Movie {Id=1 , Name="Passion of Christ", Cost=2 }
+        };
+        public List<Movie> GetAllMovies()
+        {
+            return movies; //as here movie is already hardcoded
+        }
+    }
+}
